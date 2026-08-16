d1ed276213e617551a7aac9d34759c806b97f492
thienphucta639-hash <thienphucta639@gmail.com>
2026-08-17 00:08:24 +0700

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -5,11 +5,11 @@ const withPWA = withPWAInit({
   cacheOnFrontEndNav: true,
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
-  register: true,      // MỚI THÊM: Tự động đăng ký PWA
-  skipWaiting: true,   // MỚI THÊM: Bỏ qua thời gian chờ, ép cập nhật bản offline ngay
   disable: process.env.NODE_ENV === "development",
   workboxOptions: {
     disableDevLogs: true,
+    register: true,      // Đưa vào đây
+    skipWaiting: true,   // Đưa vào đây
   },
 });
 
